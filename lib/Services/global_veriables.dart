a6ad6a57ae2f9b7c4fff11ac6fd1b3f486e646ee
Ozanakts58 <2016123050@cumhuriyet.edu.tr>
2022-07-30 10:51:51 +0300

    Initial commit

---
+++ lib/Services/global_veriables.dart
@@ -0,0 +1,3 @@
+
+String loginUrlImage = 'https://images.unsplash.com/photo-1617526738882-1ea945ce3e56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1195&q=80';
+String signUpUrlImage = 'https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg';
